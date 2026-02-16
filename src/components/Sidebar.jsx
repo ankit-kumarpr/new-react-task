db6a86bfe28770574832e215bade5ef89665e65b
ankit-kumarpr <146826335+ankit-kumarpr@users.noreply.github.com>
2026-02-16 08:38:55 +0530

    Remove useState import from Sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './Sidebar.css';
 import { NavLink } from 'react-router-dom';
 import { LayoutDashboard, X  } from 'lucide-react';
